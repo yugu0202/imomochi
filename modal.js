0265c082b1f9e211f5c9a030d43ab4386396e34e
yugu0202 <49861024+yugu0202@users.noreply.github.com>
2023-10-13 14:33:37 +0900

    add modal

---
+++ modal.js
@@ -0,0 +1,9 @@
+$(".buy").modaal({
+	overlay_close:true,//モーダル背景クリック時に閉じるか
+	before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+});
