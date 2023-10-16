994e53003714d15c18104000fb8d1fb72df361cb
yugu-0202 <applerin1119@gmail.com>
2023-10-17 03:22:50 +0900

    check api

--- modal.js
+++ modal.js
@@ -1,7 +1,7 @@
 $(".buy").modaal({
 	overlay_close:true,//モーダル背景クリック時に閉じるか
 	before_open:function(){// モーダルが開く前に行う動作
-		$('#buy').innerText = fetch("/api")
+		fetch("/api").then((response) => console.log(response))
 		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
 	},
 	after_close:function(){// モーダルが閉じた後に行う動作
