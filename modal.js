2adcb3f6a3b87babb2914d67e9d7afbb4c88b3f6
yugu-0202 <applerin1119@gmail.com>
2023-10-17 03:15:00 +0900

    add api

--- modal.js
+++ modal.js
@@ -1,6 +1,7 @@
 $(".buy").modaal({
 	overlay_close:true,//モーダル背景クリック時に閉じるか
 	before_open:function(){// モーダルが開く前に行う動作
+		$('#buy').innerText = fetch("/api")
 		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
 	},
 	after_close:function(){// モーダルが閉じた後に行う動作
