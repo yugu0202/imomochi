ab140f4c1c3fccf53eee23cac77c83754225ece6
yugu0202 <49861024+yugu0202@users.noreply.github.com>
2023-10-17 11:59:24 +0900

    fix KV name

--- functions/imomochi-status.js
+++ functions/imomochi-status.js
@@ -1,5 +1,5 @@
 export const onRequestGet = async({params, env}) => {
-    const res = await env.IMOMOCHI-STATUS.get("status");
+    const res = await env.IMOMOCHISTATUS.get("status");
     return new Response(JSON.stringify({ value: res })); 
 }
 
