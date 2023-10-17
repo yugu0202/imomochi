b6961846d862136ce796823e327aab0c1f3c1d84
yugu0202 <49861024+yugu0202@users.noreply.github.com>
2023-10-17 12:46:24 +0900

    update api

--- functions/imomochi-status.js
+++ functions/imomochi-status.js
@@ -1,8 +1,14 @@
 export const onRequestGet = async({params, env}) => {
     const res = await env.IMOMOCHISTATUS.get("status");
+    const html = `
+    <p>$(res)</p>
+    `
     return new Response(JSON.stringify({ value: res })); 
 }
 
-export async function onRequestPost(context){
+export const onRequestPost = async({request, env}) => {
+    const body = request.clone().text();
+    const data = JSON.parse(body);
     
+    await env.IMOMOCHISTATUS.put("status", data.value);
 }
