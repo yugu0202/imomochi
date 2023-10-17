88ad332720d2dbd3145151bb2057a692a2c212b1
yugu0202 <49861024+yugu0202@users.noreply.github.com>
2023-10-17 11:42:14 +0900

    async

--- functions/imomochi-status.js
+++ functions/imomochi-status.js
@@ -1,4 +1,4 @@
-export function onRequest(context){
+export async function onRequest(context){
     const res = await env.IMOMOCHI-STATUS.get("status");
     return new Response(JSON.stringify({ value: res })); 
 }
