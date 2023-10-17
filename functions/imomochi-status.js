65adc50ff5f87383260b21ba96107d025fb114a4
yugu0202 <49861024+yugu0202@users.noreply.github.com>
2023-10-17 11:41:32 +0900

    KVテスト

---
+++ functions/imomochi-status.js
@@ -0,0 +1,8 @@
+export function onRequest(context){
+    const res = await env.IMOMOCHI-STATUS.get("status");
+    return new Response(JSON.stringify({ value: res })); 
+}
+
+export function onRequestPost(context){
+    
+}
