3081ab6302213feda9fd33146da0c3b12e548a76
yugu0202 <49861024+yugu0202@users.noreply.github.com>
2023-10-17 15:57:31 +0900

    fix typo

--- functions/imomochi-status.js
+++ functions/imomochi-status.js
@@ -27,5 +27,5 @@ export const onRequestPost = async({request, env}) => {
     //const data = JSON.parse(body);
     
     //await env.IMOMOCHISTATUS.put("status", data.value);
-    return new Response(JSON.stringfy(request.body)) //.split(/(?<=^[^=]+?)=/));
+    return new Response(JSON.stringify(request.body)) //.split(/(?<=^[^=]+?)=/));
 }
